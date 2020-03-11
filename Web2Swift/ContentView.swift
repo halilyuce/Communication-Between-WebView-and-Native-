0a2a4196217d6bfc5d528529d93ea47b3b36ecc8
Halil YUCE <hllibrhmyc@gmail.com>
2020-03-11 15:08:03 +0300

    Initial Commit

---
+++ Web2Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Web2Swift
+//
+//  Created by Halil İbrahim YÜCE on 11.03.2020.
+//  Copyright © 2020 Halil İbrahim YÜCE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
